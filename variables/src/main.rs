3aae982e34d3fb3f88ce56298777d5144836fc99
m11o <m11o@hey.jp>
2022-08-27 22:53:31 +0900

    update

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,14 +1,9 @@
 fn main() {
-    let sum = 5 + 10;
-    let difference = 95.5 - 4.3;
-    let product = 4 * 30;
-    let quotient = 56.7 / 32.2;
-    let floored = 2/ 3;
-    let remainder = 43 % 5;
+    let number = 3;
 
-    let x: (i32, f64, u8) = (500, 6.4, 1);
-
-    let five_hundred = x.0;
-    let six_point_four = x.1;
-    let one = x.2;
+    if number {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
