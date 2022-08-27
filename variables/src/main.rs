49aab59f12853bb2bc13dc95a94c8223bc87ed0f
m11o <m11o@hey.jp>
2022-08-27 21:12:51 +0900

    for data type

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,14 @@
 fn main() {
-    let x = 5;
-    println!("The value of x is: {}", x);
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let floored = 2/ 3;
+    let remainder = 43 % 5;
 
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-    println!("The value of x is: {}", x);
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
 }
