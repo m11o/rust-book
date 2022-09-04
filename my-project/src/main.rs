d53a0753fa6f2729073f333483180fc5c93172b6
m11o <m11o@hey.jp>
2022-09-04 18:05:17 +0900

    About Trait

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,30 +1,40 @@
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
+pub trait Summary {
+    fn summarize(&self) -> String;
 }
 
-struct Point<T> {
-    x: T,
-    y: T,
+pub struct NewArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
-
-    let point = Point { x: 5, y: 10 };
-    let point2 = Point { x: 4.0, y: 10.0 };
+    println!("1 new tweet: {}", tweet.summarize());
 }
