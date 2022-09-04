6393fc89318a176fbfd11902b7582f611aa89e6f
m11o <m11o@hey.jp>
2022-09-04 17:41:40 +0900

    About Generic

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,5 +1,30 @@
-fn main() {
-    use std::fs::File;
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-    let f = File::open("hello.txt")?;
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let point = Point { x: 5, y: 10 };
+    let point2 = Point { x: 4.0, y: 10.0 };
 }
